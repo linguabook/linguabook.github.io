966f8c769a7f93c3bfabf06fd75df12597649153
Sergey Todyshev <stodyshev@gmail.com>
2020-11-01 13:37:16 +0700

    try to fix for SSR (gatsbyjs)

--- packages/components/poi.config.js
+++ packages/components/poi.config.js
@@ -2,7 +2,7 @@ module.exports = {
   entry: "src/index.ts",
   output: {
     format: "cjs",
-    moduleName: "WordsApp"
+    moduleName: "WordsApp",
   },
   plugins: [
     {
@@ -17,6 +17,16 @@ module.exports = {
         commonjs: "react",
         commonjs2: "react",
       },
+      "react-dom": {
+        root: "ReactDOM",
+        commonjs: "react-dom",
+        commonjs2: "react-dom",
+      },
+      "react-dom/server": {
+        root: "ReactDOMServer",
+        commonjs: "react-dom/server",
+        commonjs2: "react-dom/server",
+      },
     },
     // Some libraries import Node modules but don't use them in the browser.
     // Tell webpack to provide empty mocks for them so importing them works.
@@ -29,6 +39,9 @@ module.exports = {
       net: "empty",
       tls: "empty",
       child_process: "empty",
+    },
+    resolve: {
+      aliasFields: [],
     },
   },
 };

9e35fe18b20c47172b25ff887ca0b0bcc31e2359
Sergey Todyshev <stodyshev@gmail.com>
2021-03-19 11:45:46 +0300

    extension build script

---
+++ packages/extension/build.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+
+cd $DIR
+mkdir -p dist
+cd src
+ZIP=../dist/linguabook-extension.zip
+rm -f $ZIP
+zip $ZIP *.*

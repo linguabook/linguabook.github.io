d369e54108bfaeb8c0043462eb64bf6178c41293
Sergey Todyshev <stodyshev@gmail.com>
2022-07-05 13:39:09 +0300

    fix: fix build and update deps

--- clean.sh
+++ clean.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
 rm -rf node_modules
+rm -rf yarn.lock
 
 for D in `find ./packages -maxdepth 1 -type d`
 do
@@ -9,4 +10,6 @@ do
   rm -rf ${D}/dist
   rm -rf ${D}/lib
   rm -rf ${D}/build
+  rm -rf ${D}/yarn.lock
+  rm -rf ${D}/*.tsbuildinfo
 done
